de29353f255615b5da34a54fd03f915823511330
Dawid Sobczak <dawidsobczak@fastmail.com>
2023-03-08 17:21:34 +0000

    multithreaded working but slow"

--- src/lexer.rs
+++ src/lexer.rs
@@ -3,7 +3,7 @@ use std::fmt::{Debug, Display, Formatter};
 use crate::error::LexerError;
 
 #[derive(Debug)]
-pub enum JsonLexicalToken {
+pub enum JsonToken {
     Delim,
     Start,
     Object,
@@ -20,39 +20,91 @@ pub enum JsonLexicalToken {
     RightCurly,
     LeftCurly,
     Colon,
-    Number,
+    Number(u32),
     Bool,
     Quote,
     LeftSquareBracket,
     RightSquareBracket,
     Comma,
-    Character,
+    Character(char),
+}
+
+enum LexerState {
+    InData,
+    InString,
+    InNumber,
 }
 
 pub struct JsonLexer {
-    pub tokens: Vec<JsonLexicalToken>,
+    pub tokens: Vec<JsonToken>,
+    state: LexerState,
+    buf: String,
 }
 
 impl JsonLexer {
     pub fn new() -> JsonLexer {
         JsonLexer {
             tokens: vec![],
+            state: LexerState::InData,
+            buf: String::new(),
         }
     }
     pub fn consume(&mut self, c: char) -> Result<(), LexerError> {
-        match c {
-            'a'..='z' | 'A'..='Z' => self.tokens.push(JsonLexicalToken::Character),
-            '{' => self.tokens.push(JsonLexicalToken::LeftCurly),
-            '}' => self.tokens.push(JsonLexicalToken::RightCurly),
-            '[' => self.tokens.push(JsonLexicalToken::LeftSquareBracket),
-            ']' => self.tokens.push(JsonLexicalToken::RightSquareBracket),
-            ':' => self.tokens.push(JsonLexicalToken::Colon),
-            ',' => self.tokens.push(JsonLexicalToken::Comma),
-            '\"' => self.tokens.push(JsonLexicalToken::Quote),
-            '0'..='9' => self.tokens.push(JsonLexicalToken::Number),
-            '\n' | ' ' | '\t' => {},
-            _ => {
-                return Err(LexerError::from(format!("Unrecognized char consumed by lexer '{}'", c)));
+        let mut push = |t: JsonToken| {
+            println!("{:?}", t);
+            self.tokens.push(t);
+        };
+
+        match self.state {
+            LexerState::InData => {
+                match c {
+                    'a'..='z' | 'A'..='Z' => self.tokens.push(JsonToken::Character(c)),
+                    '{' => push(JsonToken::LeftCurly),
+                    '}' => push(JsonToken::RightCurly),
+                    '[' => push(JsonToken::LeftSquareBracket),
+                    ']' => push(JsonToken::RightSquareBracket),
+                    ':' => push(JsonToken::Colon),
+                    ',' => push(JsonToken::Comma),
+                    '\"' => {
+                        self.state = LexerState::InString;
+                        push(JsonToken::Quote);
+                    },
+                    '0'..='9' => {
+                        self.state = LexerState::InNumber;
+                        self.buf.push(c);
+                    },
+                    '\n' | ' ' | '\t' => {},
+                    _ => {
+                        return Err(LexerError::from(format!("Unrecognized char consumed by lexer '{}'", c)));
+                    }
+                }
+            }
+            LexerState::InString => {
+                match c {
+                    '\"' => {
+                        self.state = LexerState::InData;
+                        push(JsonToken::Quote);
+                    },
+                    '\n' => {
+                        return Err(LexerError::from("Cannot have newlines in strings".to_string()));
+                    },
+                    _ => push(JsonToken::Character(c)),
+                }
+            }
+            LexerState::InNumber => {
+                match c {
+                    '0'..='9' => self.buf.push(c),
+                    _ => {
+                        self.state = LexerState::InData;
+                        match self.buf.parse() {
+                            Ok(num) => {
+                                push(JsonToken::Number(num));
+                                self.buf.clear();
+                            },
+                            Err(e) => return Err(LexerError::from(format!("Cannot parse string to u32 {:?}", e))),
+                        }
+                    },
+                }
             }
         }
         return Ok(());
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,170 @@
-use std::error::Error;
-use std::fs;
-use env_logger;
-use log::{debug, error, log_enabled, info, warn, Level};
-use env_logger::Env;
+use std::collections::{HashMap};
+use tinyrand::{StdRand, RandRange};
+use std::{fs, iter, thread};
+use std::slice::Chunks;
+use std::sync::{Arc, Mutex};
+use std::sync::mpsc::Receiver;
+use std::thread::{JoinHandle, Scope, ScopedJoinHandle, spawn};
+use std::time::Instant;
+use crossbeam_deque::{Injector, Worker};
 
 mod lexer;
 mod error;
+
 use lexer::*;
 
-fn main() {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+struct LexerOutput {}
 
-    let contents = fs::read_to_string("json/1GB.json").expect("Cannot open test file.");
-    let mut lexer : JsonLexer = JsonLexer::new();
-    
-    for c in contents.chars() {
-        lexer.consume(c).unwrap();
+struct WorkUnit<'a>(BatchId, &'a str);
+
+struct ParallelLexer<'a> {
+    handles: Vec<ScopedJoinHandle<'a, ()>>,
+    connection: crossbeam_channel::Sender<bool>,
+    queue: Arc<Injector<WorkUnit<'a>>>,
+    outputs: HashMap<String, Vec<Vec<LexerOutput>>>,
+}
+
+struct ParseTree {}
+
+impl ParseTree {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+#[derive(Clone, Debug)]
+struct BatchId(String);
+
+impl<'a> ParallelLexer<'a> {
+    pub fn new(scope: &'a Scope<'a, '_>, threads: usize) -> Self {
+        let queue: Arc<Injector<WorkUnit>> = Arc::new(Injector::new());
+        let (send, recv) = crossbeam_channel::bounded(threads);
+
+        let mut handles = vec![];
+        for _ in 0..threads {
+            let reciever = recv.clone();
+            let global = queue.clone();
+
+            handles.push(scope.spawn(move || {
+                let worker: Worker<WorkUnit> = Worker::new_fifo();
+
+                let mut should_run = true;
+                while should_run {
+                    let task: Option<WorkUnit<'a>> = worker.pop().or_else(|| {
+                        iter::repeat_with(|| {
+                            global.steal_batch_and_pop(&worker)
+                        }).find(|s| !s.is_retry()).and_then(|s| s.success())
+                    });
+                    if let Some(task) = task {
+                        let mut lexer : JsonLexer = JsonLexer::new();
+
+                        for c in task.1.chars() {
+                            lexer.consume(c).unwrap();
+                        }
+                    } else if let Ok(_) = reciever.try_recv() {
+                        should_run = false;
+                    } else {
+                        continue;
+                    }
+                }
+            }));
+        }
+        return Self { connection: send, handles, queue: queue.clone(), outputs: HashMap::new() };
     }
 
-    info!("Lexer tokens size: {}KB", std::mem::size_of_val(&*lexer.tokens.as_slice()) / 1000);
+    pub fn start(&mut self) {
+    }
+
+    fn gen_key() -> String {
+        const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                            abcdefghijklmnopqrstuvwxyz";
+        const KEY_LEN: usize = 10;
+        let mut rng = StdRand::default();
+
+        let key: String = (0..KEY_LEN).map(|_| {
+            let idx = rng.next_range(0..CHARSET.len());
+            CHARSET[idx] as char
+        }).collect();
+        return key;
+    }
+
+    pub fn new_batch(&mut self) -> BatchId {
+        let mut key = Self::gen_key();
+        while self.outputs.contains_key(key.as_str()) {
+            key = Self::gen_key();
+        }
+        self.outputs.insert(key.clone(), Vec::new());
+        BatchId(key)
+    }
+
+    pub fn add_to_batch(&mut self, id: &BatchId, input: &'a str) {
+        self.queue.push(WorkUnit(id.clone(), input));
+    }
+
+    pub fn collect_batch(&mut self, id: BatchId) -> Vec<Vec<LexerOutput>> {
+        let x = self.outputs.remove(id.0.as_str());
+        return x.unwrap();
+    }
+
+    pub fn kill(self) {
+        for _ in 0..self.handles.len() {
+            self.connection.send(true);
+        }
+        for x in self.handles {
+            let _ = x.join();
+        }
+    }
+}
+
+fn parallel() {
+    let threads = 12;
+    let now = Instant::now();
+    let contents = fs::read_to_string("json/10KB.json").expect("Cannot open test file.");
+
+    let x : Vec<(usize, char)> = contents.char_indices().collect();
+    let mut indices = vec![];
+    let mut i = x.len() / threads;
+    let mut prev = 0;
+    while i < x.len() {
+        if x.get(i).unwrap().1 != '\n' {
+            i += 1;
+        } else {
+            indices.push((prev, i));
+            prev = i;
+            i += x.len() / threads;
+        }
+    }
+    drop(x);
+
+    let mut units = vec![];
+    for i in indices {
+        units.push(&contents[i.0..i.1]);
+    }
+
+    println!("Reading file : {:?}", now.elapsed());
+    let now = Instant::now();
+
+    thread::scope(|s| {
+        let mut lexer = ParallelLexer::new(s, threads);
+        let batch = lexer.new_batch();
+        for task in units {
+            lexer.add_to_batch(&batch, task);
+        }
+        lexer.collect_batch(batch);
+        lexer.kill();
+    });
+
+    println!("Lexing : {:?}", now.elapsed());
+}
+
+fn main() {
+    let contents = fs::read_to_string("test.json").expect("Cannot open test file.");
+
+    thread::scope(|s| {
+        let mut lexer = ParallelLexer::new(s, 1);
+        let batch = lexer.new_batch();
+        lexer.add_to_batch(&batch, contents.as_str());
+        lexer.collect_batch(batch);
+        lexer.kill();
+    });
 }
