9f0aac77a869e0215c6288144c43dbae0c1ee0b9
Dawid Sobczak <dawidsobczak@fastmail.com>
2023-04-13 22:28:50 +0100

    fixed benchmarks

--- benches/lexer.rs
+++ benches/lexer.rs
@@ -12,7 +12,7 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use memmap::MmapOptions;
 
 fn fair_sequential_lexing(path: &str) -> Result<(), Box<dyn Error>> {
-    let grammar = Grammar::from("json.g");
+    let grammar = Grammar::from("data/grammar/json.g");
     let mut tokens: LinkedList<Vec<u8>> = LinkedList::new();
     {
         let file = File::open(path)?;
@@ -29,7 +29,7 @@ fn fair_sequential_lexing(path: &str) -> Result<(), Box<dyn Error>> {
 }
 
 fn bench_parallel_lexing(path: &str, threads: usize) {
-    let grammar = Grammar::from("json.g");
+    let grammar = Grammar::from("data/grammar/json.g");
     let file = File::open(path).unwrap();
     let mut memmap: memmap::Mmap = unsafe { MmapOptions::new().map(&file).unwrap() };
     let chunks = core::lexer::split_mmap_into_chunks(&mut memmap, 6000).unwrap();
@@ -46,28 +46,29 @@ fn bench_parallel_lexing(path: &str, threads: usize) {
     });
 }
 
+#[rustfmt::skip]
 fn criterion_benchmark(c: &mut Criterion) {
-    // c.bench_function("lexer_1_thread_1MB", |b| b.iter(|| bench_parallel_lexing("json/1MB.json", 1)));
-    // c.bench_function("lexer_2_thread_1MB", |b| b.iter(|| bench_parallel_lexing("json/1MB.json", 2)));
-    // c.bench_function("lexer_4_thread_1MB", |b| b.iter(|| bench_parallel_lexing("json/1MB.json", 4)));
-    // c.bench_function("lexer_8_thread_1MB", |b| b.iter(|| bench_parallel_lexing("json/1MB.json", 8)));
-    // c.bench_function("lexer_16_thread_1MB", |b| b.iter(|| bench_parallel_lexing("json/1MB.json", 16)));
-    //
-    // c.bench_function("lexer_1_thread_10MB", |b| b.iter(|| bench_parallel_lexing("json/10MB.json", 1)));
-    // c.bench_function("lexer_2_thread_10MB", |b| b.iter(|| bench_parallel_lexing("json/10MB.json", 2)));
-    // c.bench_function("lexer_4_thread_10MB", |b| b.iter(|| bench_parallel_lexing("json/10MB.json", 4)));
-    // c.bench_function("lexer_8_thread_10MB", |b| b.iter(|| bench_parallel_lexing("json/10MB.json", 8)));
-    // c.bench_function("lexer_16_thread_10MB", |b| b.iter(|| bench_parallel_lexing("json/10MB.json", 16)));
+    c.bench_function("lexer_1_thread_1MB", |b| b.iter(|| bench_parallel_lexing("data/json/1MB.json", 1)));
+    c.bench_function("lexer_2_thread_1MB", |b| b.iter(|| bench_parallel_lexing("data/json/1MB.json", 2)));
+    c.bench_function("lexer_4_thread_1MB", |b| b.iter(|| bench_parallel_lexing("data/json/1MB.json", 4)));
+    c.bench_function("lexer_8_thread_1MB", |b| b.iter(|| bench_parallel_lexing("data/json/1MB.json", 8)));
+    c.bench_function("lexer_16_thread_1MB", |b| b.iter(|| bench_parallel_lexing("data/json/1MB.json", 16)));
 
-    // c.bench_function("lexer_1_thread_100MB", |b| b.iter(|| bench_parallel_lexing("json/100MB.json", 1)));
-    // c.bench_function("lexer_2_thread_100MB", |b| b.iter(|| bench_parallel_lexing("json/100MB.json", 2)));
-    // c.bench_function("lexer_4_thread_100MB", |b| b.iter(|| bench_parallel_lexing("json/100MB.json", 4)));
-    // c.bench_function("lexer_8_thread_100MB", |b| b.iter(|| bench_parallel_lexing("json/100MB.json", 8)));
-    c.bench_function("lexer_16_thread_100MB", |b| {
-        b.iter(|| bench_parallel_lexing("json/100MB.json", 16))
-    });
+    c.bench_function("lexer_1_thread_10MB", |b| b.iter(|| bench_parallel_lexing("data/json/10MB.json", 1)));
+    c.bench_function("lexer_2_thread_10MB", |b| b.iter(|| bench_parallel_lexing("data/json/10MB.json", 2)));
+    c.bench_function("lexer_4_thread_10MB", |b| b.iter(|| bench_parallel_lexing("data/json/10MB.json", 4)));
+    c.bench_function("lexer_8_thread_10MB", |b| b.iter(|| bench_parallel_lexing("data/json/10MB.json", 8)));
+    c.bench_function("lexer_16_thread_10MB", |b| b.iter(|| bench_parallel_lexing("data/json/10MB.json", 16)));
 
-    // c.bench_function("fair_sequential_lexing", |b| b.iter(|| fair_sequential_lexing("json/100MB.json")));
+    c.bench_function("lexer_1_thread_100MB", |b| b.iter(|| bench_parallel_lexing("data/json/100MB.json", 1)));
+    c.bench_function("lexer_2_thread_100MB", |b| b.iter(|| bench_parallel_lexing("data/json/100MB.json", 2)));
+    c.bench_function("lexer_4_thread_100MB", |b| b.iter(|| bench_parallel_lexing("data/json/100MB.json", 4)));
+    c.bench_function("lexer_8_thread_100MB", |b| b.iter(|| bench_parallel_lexing("data/json/100MB.json", 8)));
+    c.bench_function("lexer_16_thread_100MB", |b| { b.iter(|| bench_parallel_lexing("data/json/100MB.json", 16)) });
+
+    c.bench_function("fair_sequential_lexing", |b| b.iter(|| fair_sequential_lexing("data/json/1MB.json")));
+    c.bench_function("fair_sequential_lexing", |b| b.iter(|| fair_sequential_lexing("data/json/10MB.json")));
+    c.bench_function("fair_sequential_lexing", |b| b.iter(|| fair_sequential_lexing("data/json/100MB.json")));
 }
 
 criterion_group!(benches, criterion_benchmark);
