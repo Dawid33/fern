b66dd492573bb3c0f7574d8c77f0cd586f136a4b
Dawid Sobczak <dawidsobczak@fastmail.com>
2023-07-25 15:24:26 +0100

    can't remember what I was doing

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,12 +5,13 @@ tmux has-session -t tmux-output
 if [ $? = 0 ] 
  then
     # Dual monitor mode with extra tmux session to show output.
-    tmux respawn-window -c ~/Projects/fern -t 'tmux-output:build' -k '
-        cd build && clear && make all 2>&1 | less -R'
+    # tmux respawn-window -c ~/Projects/fern -t 'tmux-output:build' -k '
+    #     cd build && clear && make all 2>&1 | less -R'
+    echo "Wrong"
  else
     # Single monitor mode.
     tmux new-window -c ~/Projects/fern -t 'code' -a -n 'build' '
         trap "tmux kill-window" SIGINT SIGTERM
-        cargo build 2>&1 | less -R && tmux kill-window'
+        cargo build 2>&1 | less -R +F && tmux kill-window'
 fi
 
--- src/cfg.rs
+++ src/cfg.rs
@@ -1,14 +1,15 @@
 use crate::parser::fern::AstNode;
 
-pub struct ControlFlowNode {
-
-}
+pub struct ControlFlowNode {}
 
 pub struct ControlFlowGraph {}
 
 // Convert to
 impl ControlFlowGraph {
     pub fn from(ast: Box<AstNode>) -> Self {
+        match ast {
+            _ => (),
+        }
         println!("Hello, World");
         return Self {};
     }
