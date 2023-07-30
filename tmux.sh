d4c6cd677d7321893b355fa71b566de47b228a61
Dawid Sobczak <dawidsobczak@fastmail.com>
2023-07-30 17:05:56 +0100

    Updated tmux.sh
    Also removed svg file that was accidentally commited

--- tmux.sh
+++ tmux.sh
@@ -7,7 +7,9 @@ tmux has-session -t $SESSIONNAME &> /dev/null
 if [ $? != 0 ] 
  then
     cd ~/Projects/fern
-    tmux new-session -s $SESSIONNAME -n 'code' -d bash -c "hx" \; new-window -t $SESSIONNAME -n 'terminal' -d 'bash'
+    tmux new-session -s $SESSIONNAME -n 'code' -d bash -c "hx" \;\
+      new-window -t $SESSIONNAME -n 'terminal' -d 'bash' \;\
+      new-window -t $SESSIONNAME -n 'ranger' -d 'ranger'
 fi
 
 tmux attach
