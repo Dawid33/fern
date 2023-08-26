a4392b650064f53a58a961023cfed31a74a7d30e
Dawid Sobczak <dawidsobczak@fastmail.com>
2023-08-26 10:18:16 +0100

    done

--- scripts/web.sh
+++ scripts/web.sh
@@ -1,6 +1,6 @@
 
 #!/bin/sh
 
-cargo build --target=wasm32-unknown-unknown --color=always --release 2>&1 | less -R +F
+cargo build --target=wasm32-unknown-unknown --color=always --release
 
 # wasm-pack build --out-dir web/fern --target web --out-name fern
